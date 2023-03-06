cd134bf24683f3f76ea462f8bed58820251737fb
George Modrogan <george.modrogan@cerner.com>
2023-03-07 01:02:35 +0200

    enhanced index page

--- config.js
+++ config.js
@@ -1,3 +1,6 @@
 export const clientId = '7fdc0cd8-36d5-4e4d-830f-c0897f6b6458'
 export const redirectUri = 'https://gmodrogan.github.io/mgw_soh/after-auth'
 export const indexUri = 'https://gmodrogan.github.io/mgw_soh/index'
+
+// export const redirectUri = 'http://localhost:63342/mgw_soh/after-auth.html'
+// export const indexUri = 'http://localhost:63342/mgw_soh/index.html'
--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-import {Cookie} from "./helpers";
+import { Cookie } from "./helpers.js";
+
+const fhirUrl = Cookie.get("fhir_url")
+let token_data_cookie = Cookie.get('token_data')
+const token_data = token_data_cookie != undefined ? JSON.parse(token_data_cookie) : null;
 
 async function getPatient() {
-    let token_data_cookie = Cookie.get('token_data')
-    const token_data = token_data_cookie != undefined ? JSON.parse(token_data_cookie) : null;
+
     if (!token_data.access_token || !token_data.patient) {
         console.log('no access token or patient found in cookie')
         return
@@ -19,6 +22,8 @@ async function getPatient() {
 
 getPatient().then((data) => {
     console.log(data)
+    document.getElementById('mgw-data-url').textContent = fhirUrl + '/Patient/' + token_data.patient
+    document.getElementById('mgw-data-content').textContent= JSON.stringify(data)
 }).catch((err) => {
     debugger
     console.log('error fetching patient data')
