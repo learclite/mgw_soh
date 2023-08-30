63f767235f5c3f2416c4da11d33dd1c114779965
Florian N. Craciun <florianncraciun@gmail.com>
2023-08-30 15:04:38 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-export const clientId = '7fdc0cd8-36d5-4e4d-830f-c0897f6b6458' //prod
+export const clientId = '7eabc856-a2fd-4897-8982-8eb9bf5220d1' //prod
 //export const clientId = '48dc94d9-0b92-4dc2-9de7-bf2b889a97dc' // dev
 export const redirectUri = 'https://learclite.github.io/mgw_soh/after-auth'
 export const indexUri = 'https://learclite.github.io/mgw_soh/index'
