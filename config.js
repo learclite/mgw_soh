4ce08c04d3d65b2687b185b3927bf2475890faa6
George Modrogan <gmodrogan@users.noreply.github.com>
2024-02-29 10:21:14 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-// export const clientId = '7fdc0cd8-36d5-4e4d-830f-c0897f6b6458' //prod
-export const clientId = '3ecbc8c3-313d-4fbe-a64e-6d3a5adfad17' //provider-confidential
+ export const clientId = '7fdc0cd8-36d5-4e4d-830f-c0897f6b6458' //prod/int
+//export const clientId = '3ecbc8c3-313d-4fbe-a64e-6d3a5adfad17' //provider-confidential
 //export const clientId = '48dc94d9-0b92-4dc2-9de7-bf2b889a97dc' // dev
 export const redirectUri = 'https://gmodrogan.github.io/mgw_soh/after-auth'
 export const indexUri = 'https://gmodrogan.github.io/mgw_soh/index'
